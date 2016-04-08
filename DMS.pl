8e44d68ef0544907ef9a3ada727fe626fbe0438b
Kevin Quirós <kevinquiros96@gmail.com>
2016-04-07 20:42:07 -0600

    hola

--- DMS.pl
+++ DMS.pl
@@ -3,6 +3,8 @@ diabolic([1, 12, 7, 14, 8, 13, 2, 11, 10, 3, 16, 5, 15, 6, 9, 4]).
 diabolic([1, 8, 11, 14, 12, 13, 2, 7, 6, 3, 16, 9, 15, 10, 5, 4]).
 listNums(L) :- L = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16].
 
+/*-------------------- Validate Functions --------------------*/
+
 /*Checking the sum of the rows*/
 check_rows(L) :- matrix4x4(L, Y), check_rows2(Y).
 
@@ -19,6 +21,10 @@ check_columns2(A, [B|Bs]) :- check_columns2(A, B, Bs).
 check_columns2(A, B, [C|Cs]) :- check_columns2(A, B, C, Cs).
 check_columns2(A, B, C, [D|Ds]) :- check_columns2(A, B, C, D, Ds).
 check_columns2([A|As], [B|Bs], [C|Cs], [D|Ds], E) :- sum34([A, B, C, D]), check_columns2(As, Bs, Cs, Ds, E).
+
+/*checking the sum of the diagonals*/
+
+/*-------------------- Auxiliar Functions --------------------*/
 
 /*Sum is 34*/
 sum34(L):- list_sum(L, T), T==34.
